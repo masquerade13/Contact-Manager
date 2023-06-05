0531bcae6abd0fb89fad5f81b0d56820d9de4bc4
harsh davda <thepkyc@gmail.com>
2023-06-05 18:25:28 +0530

    Maybe Done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var pophide = document.getElementById('pop_up_window');
-
 var overlay = document.getElementById('whole-middle-container');
-
+let LatestImage;
 
 
 function editdel(index) {
@@ -17,6 +16,7 @@ function popdisplay() {
     pophide.style.display = 'flex';
     overlay.style.opacity = '0.2';
     document.body.style.backgroundColor = 'black';
+    LatestImage = "";
 }
 
 function poplhide() {
@@ -25,12 +25,63 @@ function poplhide() {
     document.body.style.backgroundColor = 'rgb(4, 19, 26)';
 }
 
+const reader = new FileReader();
+document.getElementById('profile-input').addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+        LatestImage = reader.result;
+    };
+});
 
 function submitted() {
+
+
     var fname = document.getElementById('fnamee').value;
     var lname = document.getElementById('lnamee').value;
     var phnum = document.getElementById('pnum').value;
     var adda = document.getElementById('addaras').value;
+
+    if (fname.length == "" && lname.length == "" && phnum.length == ""  && adda.length == "") {
+        alert("Please Fill all the fields.");
+        return;
+    }
+    if (fname.length <= 2 && fname.length > 0) {
+        alert("First Name Cannot Less than 2 characters.");
+        return;
+    }
+
+    else if (fname.length == "") {
+        alert("First Name Cannot be Empty.");
+        return;
+    }
+
+    if (lname.length <= 2 && lname.length > 0) {
+        alert("Last Name Cannot Less than 2 characters.");
+        return;
+    }
+    else if (lname.length == "") {
+        alert("Last Name Cannot be Empty.");
+        return;
+    }
+
+    if (phnum.length != 10) {
+        alert("Phone Number Must be of 10 Digits.");
+        return;
+    }
+    else if (phnum.length == "") {
+        alert("Phone Number Cannot be Empty.");
+        return;
+    }
+
+    if (adda.length <= 2 && adda.length > 0) {
+        alert("Address Cannot Less than 2 characters.");
+        return;
+    }
+    else if (adda.length == "") {
+        alert("Address Cannot be Empty.");
+        return;
+    }
 
     let ContactList;
     if (localStorage.getItem("ContactList") == null) {
@@ -38,18 +89,20 @@ function submitted() {
     } else {
         ContactList = JSON.parse(localStorage.getItem("ContactList"));
     }
+    console.log(reader.result);
+
 
     let html = `<div class="card"><ul class="itul" id="myUL">`;
 
-    html += '<li><img onclick="editdel(' + ContactList.length + ')" class="three-dot" src="./IMAGES/three-dot.svg"></li>';
+    html += '<li  class="for-pd"><img onclick="editdel(' + ContactList.length + ')" class="three-dot" src="./IMAGES/three-dot.svg"></li>';
 
-    html += '<div class="edidel-div" id="edidel-div-' + ContactList.length + '"> <div class="div-del" onclick="onndelete()">DELETE</div> <div class="div-edit"  onclick="onnedit()">EDIT</div></div>';
+    html += '<div class="edidel-div" id="edidel-div-' + ContactList.length + '"> <div class="div-del" onclick="onndelete()">DELETE</div> <div class="div-edit"  onclick="onnedit(' + ContactList.length + ')">EDIT</div></div>';
 
-    html += '<li class="center-human"><img class="human" src="./IMAGES/human.png"></li>';
-    html += `<li>FirstName: ${fname}</li>`;
-    html += `<li>LastName: ${lname}</li>`;
-    html += `<li>Phone Number: ${phnum}</li>`;
-    html += `<li>Address: ${adda}</li>`;
+    html += '<li class="center-human"><img class="human" id="human"></li>';
+    html += `<li class="for-pd for-mt">FirstName: ${fname}</li>`;
+    html += `<li class="for-pd">LastName: ${lname}</li>`;
+    html += `<li class="for-pd">Phone Number: ${phnum}</li>`;
+    html += `<li class="for-pd">Address: ${adda}</li>`;
 
     html += `</ul><div>`;
     document.getElementById("all-card-parts").innerHTML += html;
@@ -62,16 +115,16 @@ function submitted() {
 
     ContactList.push({
         firstname: fname, lastname: lname, Phonenumber: phnum,
-        Address: adda
+        Address: adda, image: LatestImage
     });
 
     localStorage.setItem('ContactList', JSON.stringify(ContactList));
 
     poplhide();
-
+    document.getElementById("contact-img-" + index).src = "";
     showdata();
-
 }
+
 
 
 function showdata() {
@@ -84,23 +137,27 @@ function showdata() {
 
     document.getElementById("all-card-parts").innerHTML = "";
     for (let index = 0; index < ContactList.length; index++) {
-        
+
         const element = ContactList[index];
-        // console.log(element.firstname + " Inputted");
         let html = `<div class="card"><ul class="itul" id="myUL">`;
 
-        html += '<li><img onclick="editdel(' + index + ')" class="three-dot" src="./IMAGES/three-dot.svg"></li>';
+        html += '<li class="for-pd"><img onclick="editdel(' + index + ')" class="three-dot" src="./IMAGES/three-dot.svg"></li>';
 
         html += '<li><div class="edidel-div" id="edidel-div-' + index + '"> <div class="div-del" onclick="onndelete(' + index + ')">DELETE</div> <div class="div-edit"  onclick="onnedit(' + index + ')">EDIT</div></div></li>';
-
-        html += '<li class="center-human"><img class="human" src="./IMAGES/human.png"></li>';
-        html += `<li>FirstName: ${element.firstname}</li>`;
-        html += `<li>LastName: ${element.lastname}</li>`;
-        html += `<li>Phone Number: ${element.Phonenumber}</li>`;
-        html += `<li>Address: ${element.Address}</li>`;
+        html += '<li class="center-human"><img class="human" id="contact-img-' + index + '" ></li>';
+        html += `<li class="for-pd for-mt">FirstName: ${element.firstname}</li>`;
+        html += `<li class="for-pd">LastName: ${element.lastname}</li>`;
+        html += `<li class="for-pd">Phone Number: ${element.Phonenumber}</li>`;
+        html += `<li class="for-pd">Address: ${element.Address}</li>`;
 
         html += `</ul><div>`;
         document.getElementById("all-card-parts").innerHTML += html;
+        if (element.image == "") {
+            document.getElementById("contact-img-" + index).src = './IMAGES/human.png';
+        }
+        else {
+            document.getElementById("contact-img-" + index).src = element.image;
+        }
     }
 }
 
@@ -113,14 +170,13 @@ function onndelete(index) {
         ContactList = JSON.parse(localStorage.getItem("ContactList"));
     }
 
-
     ContactList.splice(index, 1);
 
     localStorage.setItem("ContactList", JSON.stringify(ContactList));
     showdata();
 }
 
-
+let updateindex;
 function onnedit(index) {
     let ContactList;
     if (localStorage.getItem("ContactList") == null) {
@@ -130,38 +186,21 @@ function onnedit(index) {
     }
 
     const element = ContactList[index];
-    // console.log(element.firstname + " Displaying");
-    // console.log(element.lastname + " Displaying");
-    
 
     document.getElementById('fnamee').value = element.firstname;
     document.getElementById('lnamee').value = element.lastname;
     document.getElementById('pnum').value = element.Phonenumber;
+    document.getElementById("contact-img-" + index).src = element.image;
     document.getElementById('addaras').value = element.Address;
+
+    document.getElementById("Add-contact-btn").style.display = "none";
+    document.getElementById("edit-contact-btn").style.display = "block";
+
+    updateindex = index;
     popdisplay();
-    
-    ContactList.splice(index, 1);
-    localStorage.setItem("ContactList", JSON.stringify(ContactList));
-    showdata();
 }
 
-
-
-
-
-
-// document.onreadystatechange = function () {
-//     if (document.readyState === 'complete') {
-//         for (i = 0; i < todos.length; i++) {
-//             addTodoInUI(todos[i]);
-//         }
-//     }
-// }
-
-
-function myFunction() {
-    // Declare variables
-
+function update() {
     let ContactList;
     if (localStorage.getItem("ContactList") == null) {
         ContactList = [];
@@ -169,25 +208,62 @@ function myFunction() {
         ContactList = JSON.parse(localStorage.getItem("ContactList"));
     }
 
-    var input, filter, ul, li, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+    const element = ContactList[updateindex];
+
+    element.firstname = document.getElementById('fnamee').value;
+    element.lastname = document.getElementById('lnamee').value;
+    element.Phonenumber = document.getElementById('pnum').value;
+    element.Address = document.getElementById('addaras').value;
+
+    if (element.firstname.length <= 2 && fname.length > 0) {
+        alert("First Name Cannot Less than 2 characters...");
+        return;
     }
-  }
+    else if (element.firstname.length == "") {
+        alert("First Name Cannot be Empty...");
+        return;
+    }
+
+    if (element.lastname.length <= 2 && lname.length > 0) {
+        alert("Last Name Cannot Less than 2 characters...");
+        return;
+    }
+    else if (element.lastname.length == "") {
+        alert("Last Name Cannot be Empty...");
+        return;
+    }
+
+    if (element.Phonenumber.length != 10) {
+        alert("Phone Number Must be of 10 Digits...");
+        return;
+    }
+    else if (element.Phonenumber.length == "") {
+        alert("Phone Number Cannot be Empty...");
+        return;
+    }
+
+    if (element.Address.length <= 2 && element.Address.length > 0) {
+        alert("Address Cannot Less than 2 characters...");
+        return;
+    }
+    else if (element.Address.length == "") {
+        alert("Address Cannot be Empty...");
+        return;
+    }
+
+    if (element.image == "" && LatestImage == "") {
+        element.image = "";
+    }
+    else if (LatestImage != "") {
+        element.image = LatestImage;
+    }
+
+    localStorage.setItem("ContactList", JSON.stringify(ContactList));
+    poplhide();
+    showdata();
+    document.getElementById("Add-contact-btn").style.display = "block";
+    document.getElementById("edit-contact-btn").style.display = "none";
+}
 
 
 showdata();
-
-
